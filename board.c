acc31b6354f287aee602ca2723f329e31f8597c0
Ros <ross.shelashskyi@tcu.edu>
2023-11-12 07:50:09 -0600

    added global variables for ranks and files

--- board.c
+++ board.c
@@ -21,4 +21,22 @@ void setBoards(){
     blackPieces = BKing | BQueens | BRooks | BKnights | BBishops | BPawns;
 
     board = whitePieces | blackPieces;
+
+    aFile = 0xFF00000000000000;
+    bFile = 0x00FF000000000000;
+    cFile = 0x0000FF0000000000;
+    dFile = 0x000000FF00000000;
+    eFile = 0x00000000FF000000;
+    fFile = 0x0000000000FF0000;
+    gFile = 0x000000000000FF00;
+    hFile = 0x00000000000000FF;
+
+    rank1 = 0x8080808080808080;
+    rank2 = 0x4040404040404040;
+    rank3 = 0x2020202020202020;
+    rank4 = 0x1010101010101010;
+    rank5 = 0x0808080808080808;
+    rank6 = 0x0404040404040404;
+    rank7 = 0x0202020202020202;
+    rank8 = 0x0101010101010101;
 }
--- board.h
+++ board.h
@@ -24,5 +24,23 @@ INT BKnights;
 INT BBishops;
 INT BPawns;
 
+INT aFile;
+INT bFile;
+INT cFile;
+INT dFile;
+INT eFile;
+INT fFile;
+INT gFile;
+INT hFile;
+
+INT rank1;
+INT rank2;
+INT rank3;
+INT rank4;
+INT rank5;
+INT rank6;
+INT rank7;
+INT rank8;
+
 //function declarations
 void setBoards();
